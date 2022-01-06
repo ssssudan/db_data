0f19e24a579b81ccc38e72b7c8e9ce873fe2e457
KyeongMin Kang <kangkm0324@gmail.com>
2022-01-06 14:49:42 +0900

    초기 커밋.

---
+++ db_data.h
@@ -0,0 +1,32 @@
+﻿
+// db_data.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CdbdataApp:
+// 이 클래스의 구현에 대해서는 db_data.cpp을(를) 참조하세요.
+//
+
+class CdbdataApp : public CWinApp
+{
+public:
+	CdbdataApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CdbdataApp theApp;
---
+++ db_dataDlg.cpp
@@ -0,0 +1,204 @@
+﻿
+// db_dataDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "db_data.h"
+#include "db_dataDlg.h"
+#include "afxdialogex.h"
+#include "mysql.h"
+
+
+#define def_host_name   "127.0.0.1"     
+#define def_user_name   "super1"     
+#define def_password    "1234"      
+#define def_db_name     "test"     
+#define def_port_num    3306             
+
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CdbdataDlg 대화 상자
+
+
+
+CdbdataDlg::CdbdataDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DB_DATA_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CdbdataDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, m_List);
+}
+
+BEGIN_MESSAGE_MAP(CdbdataDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CdbdataDlg 메시지 처리기
+
+BOOL CdbdataDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	
+	MYSQL connect;
+	MYSQL_RES* sql_result;
+	MYSQL_ROW sql_row;
+
+	mysql_init(&connect);
+	if(!mysql_real_connect(&connect, def_host_name, def_user_name, def_password, def_db_name, def_port_num, (char*)NULL, 0)){
+		CString s = _T("");
+		s = mysql_error(&connect);
+		AfxMessageBox(s, MB_OK);
+	}
+	else {
+		m_List.AddString(_T("DB 연결 성공"));
+	}
+	
+	int field;
+	mysql_query(&connect, "set Names euckr");
+	mysql_query(&connect, "select * from rgb");
+	sql_result = mysql_store_result(&connect);
+	field = mysql_num_fields(sql_result);
+	if (sql_result == NULL) {
+		AfxMessageBox(_T("조회된 결과가 없다."));
+	}
+	else {
+		while ((sql_row = mysql_fetch_row(sql_result))!= NULL) {
+			CString row;
+			for (int i = 0; i < field; i++) {
+				row += sql_row[i];
+				row += " ";
+			}
+			int cnt = m_List.GetCount();
+			m_List.InsertString(cnt, row);
+		}
+	}
+
+	mysql_free_result(sql_result);
+	mysql_close(&connect);
+
+	
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CdbdataDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CdbdataDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CdbdataDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
